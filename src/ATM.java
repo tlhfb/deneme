fc8519bfcebabbb8ffa2a08aad723bdeff1e4a96
tlhfb <talhacoplu@gmail.com>
2022-01-05 19:43:03 +0300

    atm yandı

---
+++ src/ATM.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ATM {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println("a'nın 2 fazlası = "+ (a+2));
+    }
+}
